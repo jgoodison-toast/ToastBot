06de0055f400b0977d3a5c5fb46048c615555d76
Jason Goodison <jgoodison@toasttab.com>
2017-11-09 15:56:32 -0500

    response with quick reply

--- index.js
+++ index.js
@@ -91,42 +91,27 @@ app.get('/webhook', (req, res) => {
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
   let response;
-  
+  console.log("this was called")
   // Checks if the message contains text
-  if (received_message.text) {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    }
-  } else if (received_message.attachments) {
+  if (received_message.text) {
     // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
+    //let attachment_url = received_message.attachments[0].payload.url;
     response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
+        "text":"that is a test",
+         "quick_replies":[
+        {
+          "content_type":"text",
+          "title":"BUTTON MF",
+          "payload":"BUTTON MF"
+        },
+        {
+          "content_type":"text",
+          "title":"Yall mf need jesus and shit",
+          "payload":"BUTTON2 MF"
         }
+           
+      ]
       }
-    }
   } 
   
   // Send the response message
@@ -135,7 +120,20 @@ function handleMessage(sender_psid, received_message) {
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+  let response;
+  
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+  console.log(payload, "this is the payload")
 
+  // Set the response based on the postback payload
+  if (payload === 'Yes!') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'No!') {
+    response = { "text": "Oops, try sending another image." }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
